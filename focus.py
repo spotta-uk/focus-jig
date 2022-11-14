93cd2a8f7e7134a451669a8a8644e9d52a1b6cd6
jordandocker <jordan.docker@spotta.co>
2022-11-14 10:18:27 +0000

    focus.py now works for Forestpodv5 boards exclusively

--- focus.py
+++ focus.py
@@ -4,71 +4,37 @@
 Copyright Spotta Ltd. 2022
 All rights reserved.
 @author jordan.docker@spotta.co
+
+This script can focus ForestPodv5 boards (uses the black development pcb and yellow sunrise pcb)
+The forestpodv5-focus.srec firmware needs to be loaded up onto the pod prior to running this script
+For a more automated solution use the focus_automated.py script (requires a linux pc and red factory programming pcb)
 """
 
 import serial.tools.list_ports
 import cv2
 import numpy as np
 
-RX_WIDTH = 544
-RX_HEIGHT = 480
 
-ports = serial.tools.list_ports.comports()
-comport = None
-for port in ports:
-    # The UART has a specific vendor id (vid) and product id (pid)
-    if port.vid == 1027 and port.pid == 24577:
-        comport = port.device
-        break
-if comport is None:
-    message = "Error: couldn't find COM port for UART\n"
-    if len(ports) == 0:
-        message += "No ports available"
-    else:
-        message += "Available ports...\n"
-        for port in ports:
-            message += f"{port.device} {port.description.ljust(35)} vid={port.vid} pid={port.pid} \n"
-    raise Exception(message)
-ser = serial.Serial(comport)
+def main():
+    comport = find_comport()
+    ser = serial.Serial(comport)
+    ser.baudrate = 230400*4
+    ser.set_buffer_size(rx_size=256000, tx_size=12800)
+    ser.timeout = 0.100  # 100ms timeout
+    winname = "Spotta Focussing Program"
+    cv2.destroyAllWindows()
+    cv2.namedWindow(winname)
+    cv2.moveWindow(winname, 0, 0)
 
-
-ser.baudrate = 230400*4
-ser.set_buffer_size(rx_size=256000, tx_size=12800)
-ser.timeout = 0.100  # 100ms timeout
-winname = "Spotta Focussing Program"
-cv2.destroyAllWindows()
-cv2.namedWindow(winname)
-cv2.moveWindow(winname, 0, 0)
-
-try:
+    # try:
     while True:
-        if ser.isOpen() is False:
-            ser.open()
-        raw = []
-        ser.reset_input_buffer()
-        print("Waiting for data")
-        ser.write("G".encode('ascii'))
-        while ser.in_waiting == 0:
-            ser.write("G".encode('ascii'))
-        print("Data Received")
-        try:
-            for i in range(RX_WIDTH*RX_HEIGHT):
-                read = ser.read(1)
-                if len(read) != 1:
-                    # Timeout
-                    break
-                raw.append(read)
-        except:
-            ser.close()
+        raw = get_jpeg_raw_data(ser)
 
         # We have received (presumably!) jpeg data
         print("Received " + str(len(raw)) + " bytes.")
         barray = bytes([int.from_bytes(x, "big") for x in raw])
         with open("img.jpeg", "wb+") as f:
             f.write(barray)
-        # f = open("img.jpeg", "wb+")
-        # f.write(barray)
-        # f.close()
         img = cv2.imread("img.jpeg")
         if img is None:
             print("SYNC ERROR - SKIPPING FRAME!!")
@@ -123,5 +89,55 @@ try:
             cv2.imshow(winname, stitched)
             cv2.waitKey(25)
 
-except KeyboardInterrupt:
-    ser.close()
+    # except KeyboardInterrupt:
+    #     ser.close()
+
+
+def find_comport():
+    ports = serial.tools.list_ports.comports()
+    comport = None
+    for port in ports:
+        # The UART has a specific vendor id (vid) and product id (pid)
+        if port.vid == 1027 and port.pid == 24577:
+            comport = port.name
+            break
+    if comport is None:
+        message = "Error: couldn't find COM port for UART\n"
+        if len(ports) == 0:
+            message += "No ports available"
+        else:
+            message += "Available ports...\n"
+            for port in ports:
+                message += f"{port.device} {port.description.ljust(35)} vid={port.vid} pid={port.pid} \n"
+        raise Exception(message)
+    return comport
+
+
+def get_jpeg_raw_data(ser):
+    while True:
+        if ser.isOpen() is False:
+            ser.open()
+        raw = []
+        ser.reset_input_buffer()
+        print("Waiting for data", end="... ")
+        while ser.in_waiting == 0:
+            pass
+        try:
+            while True:
+                read = ser.read(1)
+                if len(read) != 1:
+                    # Timeout
+                    break
+                raw.append(read)
+                if len(raw) == 3:
+                    # Check that the raw data start with the 3 byte sequence that all jpegs start with...
+                    if raw != [b'\xff', b'\xd8', b'\xff']:
+                        raw = []
+        except Exception:
+            ser.close()
+
+        return raw
+
+
+if __name__ == "__main__":
+    main()
