e6a0cf7c5e23fd00801d731a77eafc26277817aa
Varun Karkera <varun.karkera@spotta.co>
2024-05-29 13:48:57 +0100

    modified script

--- focus_automated_LINUX_ONLY.py
+++ focus_automated_LINUX_ONLY.py
@@ -12,8 +12,8 @@ import video_feed_window
 import time
 import argparse
 import sys
-#from pystlink import PyStlink
-#from stbridge import STBridge
+from pystlink import PyStlink
+from stbridge import STBridge
 from pcbs.factory_pcb import FactoryPcb, IOExpander
 
 parser = argparse.ArgumentParser(description="This script can focus bedpod and forestpod boards (uses the red factory programming pcb and yellow sunrise pcb)")
@@ -28,30 +28,30 @@ def main():
         print("\033[91m Error:\033[0m File "+str(FOCUS_FIRMWARE) + " was not found!")
         sys.exit()
 
- #   print("This python script only works on Linux PCs. Use focus.py for Windows PCs")
- #   pystlink = PyStlink(verbosity=0)
- #   stbridge = STBridge()
- #   io_expander = IOExpander(stbridge, 0x20)
- #   factory_pcb = FactoryPcb(io_expander, quiet_mode=False)  # quiet_mode enable/disables buzzer sounds
- #   factory_pcb.turn_off_all_peripherals()
-#
- #   ser = jpeg_over_uart.get_serial_connection()
- #   video_feed_window.setup()
-#
- #   factory_pcb.turn_on_pod()
- #   time.sleep(0.1)
-#
- #   while True:
- #       raw = jpeg_over_uart.get_data(ser)
- #       if len(raw) == 0:
- #           print(f"\nPrograming pod with {FOCUS_FIRMWARE}")
- #           try:
- #               pystlink.program_flash(str(FOCUS_FIRMWARE))
- #               time.sleep(1.5)
- #           except Exception as e:
- #               pass
- #           continue
- #       video_feed_window.draw(raw)
+    print("This python script only works on Linux PCs. Use focus.py for Windows PCs")
+    pystlink = PyStlink(verbosity=0)
+    stbridge = STBridge()
+    io_expander = IOExpander(stbridge, 0x20)
+    factory_pcb = FactoryPcb(io_expander, quiet_mode=False)  # quiet_mode enable/disables buzzer sounds
+    factory_pcb.turn_off_all_peripherals()
+
+    ser = jpeg_over_uart.get_serial_connection()
+    video_feed_window.setup()
+
+    factory_pcb.turn_on_pod()
+    time.sleep(0.1)
+
+    while True:
+        raw = jpeg_over_uart.get_data(ser)
+        if len(raw) == 0:
+            print(f"\nPrograming pod with {FOCUS_FIRMWARE}")
+            try:
+                pystlink.program_flash(str(FOCUS_FIRMWARE))
+                time.sleep(1.5)
+            except Exception as e:
+                pass
+            continue
+        video_feed_window.draw(raw)
 
 
 if __name__ == "__main__":
