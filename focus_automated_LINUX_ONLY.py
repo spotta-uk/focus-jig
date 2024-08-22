41468e42d0d897a5cdfa8133a949f8e83420c264
Varun Karkera <varun.karkera@spotta.co>
2024-08-22 16:33:19 +0100

    got pyocd flashing working

--- focus_automated_LINUX_ONLY.py
+++ focus_automated_LINUX_ONLY.py
@@ -12,7 +12,7 @@ import video_feed_window
 import time
 import argparse
 import sys
-from pystlink import PyStlink
+import subprocess
 from stbridge import STBridge
 from pcbs.factory_pcb import FactoryPcb, IOExpander
 
@@ -21,16 +21,21 @@ parser = argparse.ArgumentParser(description="This script can focus bedpod and f
 parser.add_argument("--board",type=str,help="board to program and run focus jig application on")
 args = parser.parse_args()
 
+def program_flash(binary_file):
+   subprocess.run(["pyocd","load","-e", "chip","--frequency","4000khz","--target","stm32l496vgtx","-a",
+                    "0x08000000","--format","bin",binary_file])
+   subprocess.run(["pyocd","reset","--target","stm32l496vgtx"])
+
 def main():
     
-    FOCUS_FIRMWARE = Path("bin")/ (args.board + "-focus.srec")
+    FOCUS_FIRMWARE = Path("bin")/ (args.board + "-focus.bin")
 
     if not FOCUS_FIRMWARE.is_file():
         print("\033[91m Error:\033[0m File "+str(FOCUS_FIRMWARE) + " was not found!")
         sys.exit()
 
     print("This python script only works on Linux PCs. Use focus.py for Windows PCs")
-    pystlink = PyStlink(verbosity=0)
+
     stbridge = STBridge()
     io_expander = IOExpander(stbridge, 0x20)
     factory_pcb = FactoryPcb(io_expander, quiet_mode=False)  # quiet_mode enable/disables buzzer sounds
@@ -38,22 +43,20 @@ def main():
 
     ser = jpeg_over_uart.get_serial_connection()
     video_feed_window.setup()
-
     factory_pcb.turn_on_pod()
-    time.sleep(0.1)
-
+    time.sleep(1)
+    
     while True:
         raw = jpeg_over_uart.get_data(ser)
         if len(raw) == 0:
             print(f"\nPrograming pod with {FOCUS_FIRMWARE}")
             try:
-                pystlink.program_flash(str(FOCUS_FIRMWARE))
+                program_flash(FOCUS_FIRMWARE)
                 time.sleep(1.5)
             except Exception as e:
                 pass
             continue
         video_feed_window.draw(raw)
-
 
 if __name__ == "__main__":
     main()
